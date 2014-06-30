52b2042388f51313d51cab82d6e101a7fa5e48d4
Craig Tack <craigtack@gmail.com>
2014-06-30 16:09:55 -0400

    removed keyHandler.js. Handler.js used as replacement

--- hangman/public/js/keyHandler.js
+++
@@ -1,7 +0,0 @@
-$(document).keypress(function(e) {
-	var code = e.keyCode || e.which;
-	if (code > 64 && code < 91 || code > 96 && code < 123) {
-		var letter = String.fromCharCode(code).toUpperCase();
-		document.getElementById(letter).click();
-	}
-});
