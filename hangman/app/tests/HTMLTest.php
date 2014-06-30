99dd2543794a643acd433cfcc40cf53044881bd2
Craig Tack <craigtack@gmail.com>
2014-06-30 15:59:16 -0400

    added namespace and refactored

--- hangman/app/tests/HTMLTest.php
+++ hangman/app/tests/HTMLTest.php
@@ -1,19 +1,19 @@
 <?php
 
-use Hangman\HTML as Hangman;
+namespace Hangman\Tests;
 
-class HTMLTest extends TestCase {
+use Hangman\Classes\HTML;
+
+class HTMLTest extends \TestCase {
 	public function test_td_function_passing_empty_array() {
-		$result = Hangman\HTML::td(array());
+		$result = HTML::td(array());
 
 		$this->assertEquals('', $result);
 	}
 
 	public function test_td_function_passing_filled_array() {
-		$test_array = array('one', 'two', 'three');
-
 		$expected_result = '<td>one</td><td>two</td><td>three</td>';
-		$actual_result = Hangman\HTML::td($test_array);
+		$actual_result = HTML::td(array('one', 'two', 'three'));
 
 		$this->assertEquals($expected_result, $actual_result);
 	}
