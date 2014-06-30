38ee6b017cad02c201ff3a70982365df4fc9a0d0
Craig Tack <craigtack@gmail.com>
2014-06-30 16:07:31 -0400

    added route group

--- hangman/app/routes.php
+++ hangman/app/routes.php
@@ -11,4 +11,6 @@
 |
 */
 
-Route::get('/', array('as' => 'index', 'uses' => 'IndexController@get_index'));
+Route::group(array('namespace' => 'Hangman\Controllers'), function() {
+	Route::get('/', array('as' => 'index', 'uses' => 'IndexController@get_index'));
+});
