e48c17a67b2cd384c2201377bfbcef6483925aa7
Craig Tack <craigtack@gmail.com>
2014-06-30 15:54:37 -0400

    fixed namespace and removed getters and setters

--- hangman/app/models/Record.php
+++ hangman/app/models/Record.php
@@ -1,27 +1,13 @@
-<?php namespace Hangman\Record;
+<?php 
+
+namespace Hangman\Models;
 
 class Record {
-	private $games_won;
-	private $games_lost;
+	public $games_won;
+	public $games_lost;
 
 	public function __construct() {
 		$this->games_won = 0;
 		$this->games_lost = 0;
-	}
-
-	public function get_games_won() {
-		return $this->games_won;
-	}
-
-	public function increase_games_won() {
-		$this->games_won++;
-	}
-
-	public function get_games_lost() {
-		return $this->games_lost;
-	}
-
-	public function increase_games_lost() {
-		$this->games_lost++;
 	}
 }
