d2459adfafab07801a1e6ca286563e7d70da970e
Craig Tack <craigtack@gmail.com>
2014-07-01 17:12:19 -0400

    added new tests and refactoring

--- hangman/app/tests/IndexControllerTest.php
+++ hangman/app/tests/IndexControllerTest.php
@@ -5,310 +5,270 @@ namespace Hangman\Tests;
 use \Session as Session;
 
 class IndexControllerTest extends \TestCase {
-	public function test_new_word_received_after_new_game_clicked() {
-		// begin game
+	/*
+	VIEW
+	*/
+	public function test_view_has_gallow_stage() {
+		$this->get('/');
+
+		$this->assertViewHas('gallow_stage');
+	}
+
+	public function test_view_has_games_won() {
+		$this->get('/');
+
+		$this->assertViewHas('games_won');
+	}
+
+	public function test_view_has_games_lost() {
+		$this->get('/');
+
+		$this->assertViewHas('games_lost');
+	}
+
+	public function test_view_has_result_message() {
+		$this->get('/');
+
+		$this->assertViewHas('result_message');
+	}
+
+	public function test_view_has_word_cells() {
+		$this->get('/');
+
+		$this->assertViewHas('word_cells');
+	}
+
+	public function test_view_has_letters_guessed() {
+		$this->get('/');
+
+		$this->assertViewHas('letters_guessed');
+	}
+
+
+	/*
+	SESSION
+	*/
+	public function test_session_has_game() {
+		$this->get('/');
+
+		$this->assertSessionHas('game');
+	}
+
+
+	/*
+	FUNCTIONAL
+	*/
+	public function test_new_word_received_after_refresh() {
 		$this->get('/');
 		$original_word = unserialize(Session::get('game'))->hangman->word;
-		// simulate clicking the new game button
+
+		// refresh
 		$this->get('/');
 		$new_word = unserialize(Session::get('game'))->hangman->word;
 
 		$this->assertNotEquals($original_word, $new_word);
 	}
 
-	public function test_new_word_received_after_letters_filled_in_and_then_new_game_clicked() {
-		// begin game
+	public function test_new_word_received_after_letters_filled_in_and_then_refresh() {
 		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$original_word = $game->hangman->word;
-		$letter_in_word = substr($game->hangman->word, 0, 1);
+		$original_word = unserialize(Session::get('game'))->hangman->word;
+		$letter_in_word = substr($original_word, 0, 1);
+
 		// guess correct letter
 		$this->get('/?letter=' . $letter_in_word);
-		// simulate clicking the new game button
+
+		// refresh
 		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$new_word = $game->hangman->word;
+		$new_word = unserialize(Session::get('game'))->hangman->word;
 
 		$this->assertNotEquals($original_word, $new_word);
 	}
 
-	public function test_statistics_persist_when_new_game_clicked() {
-		// begin game
+	public function test_statistics_persist_after_refresh() {
 		$this->get('/');
+
 		// guess random letter
 		$this->get('/?letter=A');
+
 		$game = unserialize(Session::get('game'));
 		$game->record->games_won++;
 		Session::put('game', serialize($game));
+
 		// guess another random letter, game will now be complete
 		$this->get('/?letter=E');
-		// simulate clicking the new game button
+		
+		// refresh
 		$this->get('/');
-		$game = unserialize(Session::get('game'));
+		$games_won = unserialize(Session::get('game'))->record->games_won;
 
-		$result = $game->record->games_won;
+		$this->assertEquals(1, $games_won);
+	}
 
-		$this->assertEquals(1, $result);
+	public function test_games_lost_variable_increases_after_refresh() {
+		$this->get('/');
+
+		// refresh
+		$this->get('/');
+
+		$games_lost = unserialize(Session::get('game'))->record->games_lost;
+
+		$this->assertEquals(1, $games_lost);
 	}
 
 	public function test_games_won_variable_increases_on_win() {
-		// begin game
-		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$word = $game->hangman->word;
-		$chars = str_split($word);
-		// fill all but one letter in word
-		$length = count($chars);
-		for ($i = 1; $i < $length; $i++) {
-			$game->hangman->update_word_cells($i, $chars[$i]);
-		}
-		Session::put('game', serialize($game));
-		// guess final letter resulting in game win
-		$this->get('/?letter=' . $chars[0]);
-		$game = unserialize(Session::get('game'));
+		$this->winGame();
+		
+		$games_won = unserialize(Session::get('game'))->record->games_won;
 
-		$result = $game->record->games_won;
-
-		$this->assertEquals(1, $result);
+		$this->assertEquals(1, $games_won);
 	}
 
 	public function test_success_message_displayed_on_game_win() {
-		// begin game
-		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$word = $game->hangman->word;
-		$chars = str_split($word);
-		// fill all but one letter in word
-		$length = count($chars);
-		for ($i = 1; $i < $length; $i++) {
-			$game->hangman->update_word_cells($i, $chars[$i]);
-		}
-		Session::put('game', serialize($game));
-		// guess final letter resulting in game win
-		$response = $this->get('/?letter=' . $chars[0]);
+		$response = $this->winGame()['response'];
 
 		$this->assertContains('success', $response->getContent());
 	}
 
-	public function test_games_lost_variable_increases_on_game_loss() {
-		// begin game
+	public function test_games_lost_variable_persists_after_winning_game_and_beginning_new_game() {
+		$this->winGame();
+
+		// begin new game
 		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$word = $game->hangman->word;
-		$alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
 
-		$incorrect_guesses = 0;
-		$incorrect_letter = ''; // final letter that will result in game loss
-		$length = count($alphabet);
-		for ($i = 0; $i < $length; $i++) {
-			// find character not in word
-			if (strpos($word, $alphabet[$i]) === false) {
-				// don't set final incorrect letter
-				if ($incorrect_guesses == 9) {
-					$incorrect_letter = $alphabet[$i];
-					break;
-				}
+		$games_lost = unserialize(Session::get('game'))->record->games_lost;
 
-				$game->hangman->check_letter($alphabet[$i]);
-				$incorrect_guesses++;
-			}
-		}
+		$this->assertEquals(0, $games_lost);
+	}
 
-		Session::put('game', serialize($game));
-		// guess final incorrect letter resulting in game loss
-		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
+	public function test_games_lost_variable_increases_on_game_loss() {
+		$this->loseGame();
 
-		$result = $game->record->games_lost;
+		$games_lost = unserialize(Session::get('game'))->record->games_lost;
 
-		$this->assertEquals(1, $result);
+		$this->assertEquals(1, $games_lost);
 	}
 
 	public function test_failure_message_displayed_on_game_loss() {
-		// begin game
-		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$word = $game->hangman->word;
-		$alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
-
-		$incorrect_guesses = 0;
-		$incorrect_letter = ''; // final letter that will result in game loss
-		$length = count($alphabet);
-		for ($i = 0; $i < $length; $i++) {
-			// find character not in word
-			if (strpos($word, $alphabet[$i]) === false) {
-				// don't set final incorrect letter
-				if ($incorrect_guesses == 9) {
-					$incorrect_letter = $alphabet[$i];
-					break;
-				}
-
-				$game->hangman->check_letter($alphabet[$i]);
-				$incorrect_guesses++;
-			}
-		}
-
-		Session::put('game', serialize($game));
-		// guess final incorrect letter resulting in game loss
-		$response = $this->get('/?letter=' . $incorrect_letter);
+		$response = $this->loseGame();
 
 		$this->assertContains('failure', $response->getContent());	
 	}
 
-	public function test_incorrect_guesses_not_affected_by_page_refresh() {
-		// begin game
+	public function test_games_lost_variable_persists_after_losing_game_and_beginning_new_game() {
+		$this->loseGame();
+
+		// begin new game
 		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$word = $game->hangman->word;
-		$alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
 
-		$incorrect_letter = '';
-		$length = count($alphabet);
-		for ($i = 0; $i < $length; $i++) {
-			// find a letter that doesn't exist in the word
-			if (strpos(substr($alphabet[$i], $i, 1), $word) === false) {
-				$incorrect_letter = $alphabet[$i];
-				break;
-			}
-		}
+		$games_lost = unserialize(Session::get('game'))->record->games_lost;
 
-		// make incorrect guess
+		$this->assertEquals(1, $games_lost);
+	}
+
+	public function test_incorrect_guesses_not_affected_by_refresh() {
+		$incorrect_letter = $this->guessIncorrectLetter();
+
+		$original_incorrect_guesses = unserialize(Session::get('game'))->hangman->incorrect_guesses;
+
+		// refresh
 		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
-		$expected_result = $game->hangman->incorrect_guesses;
-		// page refresh
-		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
-		$actual_result = $game->hangman->incorrect_guesses;
 
-		$this->assertEquals($expected_result, $actual_result);
+		$new_incorrect_guesses = unserialize(Session::get('game'))->hangman->incorrect_guesses;
+
+		$this->assertEquals($original_incorrect_guesses, $new_incorrect_guesses);
 	}
 
 	public function test_gallow_goes_to_next_stage_after_incorrect_guess() {
-		// begin game
-		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$initial_stage = $game->hangman->gallow_stage;
-		$word = $game->hangman->word;
-		$alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
+		$this->guessIncorrectLetter();
 
-		$incorrect_letter = '';
-		$length = count($alphabet);
-		for ($i = 0; $i < $length; $i++) {
-			// find a letter that doesn't exist in the word
-			if (strpos($word, $alphabet[$i]) === false) {
-				$incorrect_letter = $alphabet[$i];
-				break;
-			}
-		}
+		$gallow_stage = unserialize(Session::get('game'))->hangman->gallow_stage;
 
-		// make incorrect guess
-		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
-		$next_stage = $game->hangman->gallow_stage;
-
-		$this->assertNotEquals($initial_stage, $next_stage);
+		$this->assertEquals(1, $gallow_stage);
 	}
 
 	public function test_gallow_maintains_stage_when_page_refreshed_with_incorrect_letter() {
-		// begin game
-		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$word = $game->hangman->word;
-		$alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
-		
-		$incorrect_letter = '';
-		$length = count($alphabet);
-		for ($i = 0; $i < $length; $i++) {
-			// find a letter that doesn't exist in the word
-			if (strpos(substr($alphabet[$i], $i, 1), $word) == false) {
-				$incorrect_letter = $alphabet[$i];
-				break;
-			}
-		}
+		$incorrect_letter = $this->guessIncorrectLetter();
 
-		// make incorrect guess
+		$initial_stage = unserialize(Session::get('game'))->hangman->gallow_stage;
+
+		// refresh
 		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
-		$initial_stage = $game->hangman->gallow_stage;
-		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
-		$next_stage = $game->hangman->gallow_stage;
+
+		$next_stage = unserialize(Session::get('game'))->hangman->gallow_stage;
 
 		$this->assertEquals($initial_stage, $next_stage);
 	}
 
 	public function test_gallow_stage_resets_after_game_loss() {
-		// begin game
+		$this->loseGame();
+
+		// begin new game
 		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$word = $game->hangman->word;
-		$alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
 
-		$incorrect_guesses = 0;
-		$incorrect_letter = ''; // final letter that will result in game loss
-		$length = count($alphabet);
-		for ($i = 0; $i < $length; $i++) {
-			// find character not in word
-			if (strpos($word, $alphabet[$i]) === false) {
-				// don't set final incorrect letter
-				if ($incorrect_guesses == 9) {
-					$incorrect_letter = $alphabet[$i];
-					break;
-				}
+		$gallow_stage = unserialize(Session::get('game'))->hangman->gallow_stage;
 
-				$game->hangman->check_letter($alphabet[$i]);
-				$incorrect_guesses++;
-			}
-		}
-
-		Session::put('game', serialize($game));
-		// guess final incorrect letter resulting in game loss
-		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
-		$initial_gallow = $game->hangman->gallow_stage;
-		// start new game
-		$this->get('/');
-		$game = unserialize(Session::get('game'));
-		$next_gallow = $game->hangman->gallow_stage;
-
-		$this->assertNotEquals($initial_gallow, $next_gallow);
+		$this->assertEquals(0, $gallow_stage);
 	}
 
 	public function test_guessing_letter_with_no_session_redirects_to_index() {
 		$response = $this->get('/?letter=A');
-		$redirect = 'Redirecting to http://localhost';
 
-		$this->assertContains($redirect, $response->getContent());
+		$this->assertRedirectedTo('/');
 	}
 
 	public function test_refreshing_page_after_win_maintains_games_won_variable() {
-		// begin game
+		$refresh_uri = $this->winGame()['refresh_uri'];
+
+		$original_games_won = unserialize(Session::get('game'))->record->games_won;
+
+		// refresh
 		$this->get('/');
+
+		$new_games_won = unserialize(Session::get('game'))->record->games_won;
+
+		$this->assertEquals($original_games_won, $new_games_won);
+	}
+
+	public function test_refreshing_page_after_loss_maintains_games_lost_variable() {
+		$incorrect_letter = $this->loseGame();
+
+		$original_games_lost = unserialize(Session::get('game'))->record->games_lost;
+
+		// refresh
+		$this->get('/?letter=' . $incorrect_letter);
+
+		$new_games_lost = unserialize(Session::get('game'))->record->games_lost;
+
+		$this->assertEquals($original_games_lost, $new_games_lost);
+	}
+
+
+	/*
+	helper funcs
+	*/
+	private function winGame() {
+		$this->get('/');
+
 		$game = unserialize(Session::get('game'));
 		$word = $game->hangman->word;
 		$chars = str_split($word);
+
 		// fill all but one letter in word
 		$length = count($chars);
 		for ($i = 1; $i < $length; $i++) {
 			$game->hangman->update_word_cells($i, $chars[$i]);
 		}
-		Session::put('game', serialize($game));
-		// guess final letter resulting in game win
-		$this->get('/?letter=' . $chars[0]);
-		$game = unserialize(Session::get('game'));
-		$initial_result = $game->record->games_won;
-		// page refresh
-		$this->get('/?letter=' . $chars[0]);
-		$game = unserialize(Session::get('game'));
-		$next_result = $game->record->games_won;
 
-		$this->assertEquals($initial_result, $next_result);
+		Session::put('game', serialize($game));
+
+		// guess final letter resulting in game win
+		return array('response' => $this->get('/?letter=' . $chars[0]),
+						'refresh_uri' => '/?letter=' . $chars[0]);
 	}
 
-	public function test_refreshing_page_after_loss_maintains_games_lost_variable() {
-		// begin game
+	private function loseGame() {
 		$this->get('/');
+
 		$game = unserialize(Session::get('game'));
 		$word = $game->hangman->word;
 		$alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
@@ -331,15 +291,30 @@ class IndexControllerTest extends \TestCase {
 		}
 
 		Session::put('game', serialize($game));
-		// guess final incorrect letter resulting in game loss
-		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
-		$initial_result = $game->record->games_lost;
-		// page refresh
-		$this->get('/?letter=' . $incorrect_letter);
-		$game = unserialize(Session::get('game'));
-		$after_result = $game->record->games_lost;;
 
-		$this->assertEquals($initial_result, $after_result);
+		// guess final incorrect letter resulting in game loss
+		return $this->get('/?letter=' . $incorrect_letter);
+	}
+
+	private function guessIncorrectLetter() {
+		$this->get('/');
+
+		$word = unserialize(Session::get('game'))->hangman->word;
+		$alphabet = str_split('abcdefghijklmnopqrstuvwxyz');
+
+		$incorrect_letter = '';
+		$length = count($alphabet);
+		for ($i = 0; $i < $length; $i++) {
+			// find a letter that doesn't exist in the word
+			if (strpos($word, $alphabet[$i]) === false) {
+				$incorrect_letter = $alphabet[$i];
+				break;
+			}
+		}
+
+		// make incorrect guess
+		$this->get('/?letter=' . $incorrect_letter);
+
+		return $incorrect_letter;
 	}
 }
